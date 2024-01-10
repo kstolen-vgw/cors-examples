e8a6442ce7ac47b11b7325073ab20fcbc3b5f5ba
Kristian Stolen <103400331+kstolen-vgw@users.noreply.github.com>
2024-01-10 10:49:14 +0800

    Add routes to auth api.

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 const router = express()
 
@@ -6,7 +7,17 @@ router.get('/healthcheck', (req, res, next) => {
     res.send(200).json()
 })
 
-router.post('/login', express.json(), (req, res, next) => {
+const allowAllOrigins = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', req.get('origin') ?? 'localhost')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    next()
+}
+
+router.use(cookieParser())
+
+router.options('/login', allowAllOrigins)
+router.post('/login', express.json(), allowAllOrigins, (req, res, next) => {
     const username = req.body.username
     const password = req.body.password
 
@@ -14,7 +25,32 @@ router.post('/login', express.json(), (req, res, next) => {
         return res.sendStatus(400)
     }
 
-    res.sendStatus(200)
+    res.cookie('custom-auth-cookie', 'why-hello-there', {
+        secure: false,
+        httpOnly: true,
+        maxAge: 1_000_000
+    })
+
+    res.status(200).send('Login OK')
+})
+
+router.post('/logout', allowAllOrigins, (req, res, next) => {
+    res.clearCookie('custom-auth-cookie')
+
+    res.status(200).send('logged out')
+})
+
+router.get('/authenticated-endpoint', allowAllOrigins, (req, res, next) => {
+    const authCookie = req.cookies['custom-auth-cookie']
+
+    if (!authCookie) {
+        return res.status(401).send('unauthenticated request')
+    }
+
+    res.status(200).json({
+        givenName: 'John',
+        surname: 'Doe',
+    })
 })
 
 const PORT = 8001
